5229e67e06835401bac5c7ea630258dacc017231
Weiran Xu <thuxwr@gmail.com>
2018-11-29 11:31:38 -0500

    Modify configure.h

--- Configure/Configure.h
+++ Configure/Configure.h
@@ -12,13 +12,54 @@
 #define LowBoundary -35 //Low boundary for decay spectrum respect to endpoint, in unit:eV
 #define UpBoundary 5 //Up boundary for decay spectrum respect to endpoint, in unit:eV
 
-#define NbinsDetSpec 70 //Nbins for detected spectrum
+#define NVoltageMax 70 //Nbins for detected spectrum
 #define DetLow -30 //Low boundary for detected spectrum. Should be larger than boundary for decay spectrum.
 #define DetUp 5 //Up boundary for detected spectrum.
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
 
 class KATRIN
 {
 	public:
+		KATRIN() {
+			char* KATRIN_path = getenv("KATRIN");
+			if(KATRIN_path==0) {
+				cout << "Environment variable 'KATRIN' is not defined." << endl;
+				exit(0);
+			}
+
+			ifstream data(((string)KATRIN_path + "/Data/data.dat").c_str());
+			if(!(data.is_open())) {
+				cout << "Data file $KATRIN/Data/data.dat cannot be opened." << endl;
+				exit(0);
+			}
+			string line;
+			Nbins = 0;
+			while(getline(data, line)) {
+				char f = line[0];
+				if(f=='#') continue;
+				if(Nbins>NVoltageMax) {
+					cout << "Data size exceed maximum." << endl;
+					exit(0);
+				}
+				istringstream iss(line);
+				iss >> Voltage[Nbins] >> Count[Nbins] >> Time[Nbins];
+				Nbins ++;
+			}
+
+			for(int i=0; i<Nbins; i++) {
+				Rate[i] = Count[i]/Time[i]*86400.;
+				Error[i] = sqrt(Count[i])/Time[i]*86400.;
+			}
+		}
+
+		~KATRIN(){}
+
 		/* Electromagnetic field. */
 		double B_A = 2.68; // in unit:Gauss
 		double B_S = 2.52e4; // in unit:Gauss
@@ -33,12 +74,24 @@ class KATRIN
 		double E_0_center = 18.574e3;
 
 		/* Measurement time distribution, in unit:day */
-		double Time[NbinsDetSpec] = {
+		/*
+		double Time[NVoltageMax] = {
 			5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
 			5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
 			5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 130, 130, 130, 130, 20, 20, 20, 20, 20, 20,
 			20, 20, 20, 20, 20, 20, 20, 20, 20, 20
 			};
+		*/
+
+		/* Data. */
+		int Nbins;
+		double Time[NVoltageMax];
+		double Voltage[NVoltageMax];
+		double Count[NVoltageMax];
+
+		/* Transformed data. */
+		double Rate[NVoltageMax];
+		double Error[NVoltageMax];
 
 		/* Expected signal rate and background rate, for Monte Carlo generation. */
 		double Sig_rate = 27.54 * 86400; // For U=18544.25V, 27.54Hz
